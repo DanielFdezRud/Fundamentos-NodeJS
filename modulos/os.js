c86a8d6135f92a4006cd73c3df2b7d8e184f962e
praxul <danielfernandez3112@gmail.com>
2020-12-08 18:24:02 +0100

    Modulo OS para saber información de bajo nivel

---
+++ modulos/os.js
@@ -0,0 +1,39 @@
+const os = require("os")
+
+// Arquitectura del OS
+// console.log(os.arch())
+
+// Plataforma del OS
+// console.log(os.platform())
+
+// Acceder a la información de las CPUs
+// console.log(os.cpus())
+
+// Visualizar los errores del sistema
+// console.log(os.constants)
+
+// // Consultar la memoria libre del PC
+// const SIZE = 1024
+// function kb(bytes){ return bytes / SIZE }
+// function mb(bytes){ return kb(bytes) / SIZE }
+// function gb(bytes){ return mb(bytes) / SIZE }
+
+// console.log(os.freemem())
+// console.log(kb(os.freemem()))
+// console.log(mb(os.freemem()))
+// console.log(gb(os.freemem()))
+
+// // Consultar la memoria del PC
+// console.log(gb(os.totalmem()))
+
+// // Carpeta del usuario
+// console.log(os.homedir())
+
+// // Carpeta de archivos temporales
+// console.log(os.tmpdir())
+
+// // Nombre de la maquina
+// console.log(os.hostname())
+
+// // Acceder a la tarjeta de red
+// console.log(os.networkInterfaces())
